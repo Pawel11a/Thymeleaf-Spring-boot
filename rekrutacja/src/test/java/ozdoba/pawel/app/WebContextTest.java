d3adface60b1d9401e203d0c49070aa4ebe2dd82
Pawel11a <Pawel15tii1@o2.pl>
2019-03-08 00:20:46 +0100

    app

---
+++ rekrutacja/src/test/java/ozdoba/pawel/app/WebContextTest.java
@@ -0,0 +1,16 @@
+package ozdoba.pawel.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+public class WebContextTest
+{
+
+    @Test
+    public void contextLoads() {
+    }
+}
